7560c0c99638476ee7e79e0c26d51d4f7beedcf8
shivanshudhamija01 <shivanshud@alantrixgeim.com>
2026-06-02 20:09:14 +0530

    Player Clamp

--- Assets/UFO Smash/_Scripts/GamePlay/Player/PlayerMovement.cs
+++ Assets/UFO Smash/_Scripts/GamePlay/Player/PlayerMovement.cs
@@ -5,14 +5,26 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private Animator animator;
 
-    private readonly int isWalkingHash = Animator.StringToHash("IsWalking");
+    [Header("Clamp Settings")]
+    [SerializeField] private float padding = 0.5f;
+
+    private readonly int isWalkingHash =
+        Animator.StringToHash("IsWalking");
 
     private IEventBus eventBus;
     private int moveDirection;
 
+    private float minX;
+    private float maxX;
+
     private void Awake()
     {
         eventBus = ServiceLocator.Get<IEventBus>();
+    }
+
+    private void Start()
+    {
+        CalculateScreenBounds();
     }
 
     private void OnEnable()
@@ -34,8 +46,44 @@ public class PlayerMovement : MonoBehaviour
     {
         bool isMoving = moveDirection != 0;
 
-        transform.Translate(Vector3.right * moveDirection * speed * Time.deltaTime);
+        transform.Translate(
+            Vector3.right *
+            moveDirection *
+            speed *
+            Time.deltaTime);
 
-        animator.SetBool(isWalkingHash, isMoving);
+        ClampPosition();
+
+        animator.SetBool(
+            isWalkingHash,
+            isMoving);
+    }
+
+    private void CalculateScreenBounds()
+    {
+        Camera cam = Camera.main;
+
+        Vector3 left =
+            cam.ViewportToWorldPoint(
+                new Vector3(0f, 0.5f, cam.nearClipPlane));
+
+        Vector3 right =
+            cam.ViewportToWorldPoint(
+                new Vector3(1f, 0.5f, cam.nearClipPlane));
+
+        minX = left.x + padding;
+        maxX = right.x - padding;
+    }
+
+    private void ClampPosition()
+    {
+        Vector3 pos = transform.position;
+
+        pos.x = Mathf.Clamp(
+            pos.x,
+            minX,
+            maxX);
+
+        transform.position = pos;
     }
 }
